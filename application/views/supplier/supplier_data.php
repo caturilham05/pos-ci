cc1e6043ab2dbff63cc3988154e016244f4cf292
unknown <caturilham05@gmail.com>
2020-04-20 23:25:21 +0700

    project CI

---
+++ application/views/supplier/supplier_data.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row pt-3">
+        <div class="col">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, distinctio itaque veniam laboriosam, ab assumenda quaerat suscipit, dolorem dolorum necessitatibus neque quasi mollitia ullam non minima quisquam soluta consequatur. Rem doloremque quibusdam, quasi delectus non beatae ut, voluptas corporis sunt laudantium ex molestiae aperiam ullam minus necessitatibus eos qui debitis? Consequatur magnam sunt dolorum. Corporis, obcaecati aliquid repellendus labore dolore facere qui iste praesentium dolores laboriosam provident illum ut id perferendis quos sapiente magni recusandae soluta sunt architecto iusto esse aut fugiat. Itaque maxime architecto illo, porro quod eligendi deserunt ad! Error optio doloremque repudiandae nulla at harum fuga quisquam.
+        </div>
+    </div>
+</div>
